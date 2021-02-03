3c8b2cb463041a7f94454955c3c49545d10c2726
filipecamargos <filipe.atrabalho@gmail.com>
2021-02-02 21:47:54 -0700

    Method Implemented

--- src/ferreira/ReverseString.java
+++ src/ferreira/ReverseString.java
@@ -19,6 +19,28 @@ public class ReverseString {
         stringList.add("Bob");
         stringList.add("reversed");
 
+        for(String n : stringList) {
+            reverseString(n);
+        }
+    }
 
+    /**
+     * Reverse the passed string
+     * @param n for the string to be reversed
+     */
+    static private void reverseString(String n) {
+
+        //Create a String Builder to manipulate the char
+        StringBuilder reversed = new StringBuilder(n);
+
+        //Loop thought and a reversed order to assigned the proper values
+        int backWard = n.length() - 1;
+
+        for (int forWard = 0; forWard < n.length(); forWard++, backWard--) {
+            reversed.setCharAt(forWard, n.charAt(backWard));
+        }
+
+        //Display the new String
+        System.out.println(n + " : " + reversed.toString());
     }
 }
