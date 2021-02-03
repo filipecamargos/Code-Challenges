8e4c4038593957909383da56bb3075168344ff49
filipecamargos <filipe.atrabalho@gmail.com>
2021-02-02 21:34:11 -0700

    expecifications

--- src/ferreira/ReverseString.java
+++ src/ferreira/ReverseString.java
@@ -1,4 +1,11 @@
 package ferreira;
 
+import java.util.*;
+
+/**
+ * Given a string, write a method that return the reverse of it
+ * Do not use any algorithm that has over linear time complexity
+ */
 public class ReverseString {
+
 }
