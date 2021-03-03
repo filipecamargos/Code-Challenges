953b9e14ae82b87e9e9a23fe702076c1712ad6b9
filipecamargos <filipe.atrabalho@gmail.com>
2021-03-02 22:18:22 -0700

    Anagrams chalenge file created

--- src/ferreira/Anagrams.java
+++ src/ferreira/Anagrams.java
@@ -11,7 +11,10 @@ public class Anagrams {
         String b = scan.next();
         scan.close();
         boolean ret = isAnagram(a, b);
+        boolean retSec = isAnagramUsingMap(a, b);
+
         System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
+        System.out.println( (retSec) ? "Anagrams" : "Not Anagrams" );
     }
 
     //Solution without using abstract types
@@ -36,8 +39,43 @@ public class Anagrams {
         return false;
     }
 
-    //Solution using Map
+    //Solution using Map with Linear Time
+    static boolean isAnagramUsingMap(String a, String b){
+        //Check based on size
+        if(a.length() != b.length()) {
+            return false;
+        }
 
+        //Create a Map to hold key and value
+        Map<Character, Integer> frequencyMapA = new HashMap<>();
+        Map<Character, Integer> frequencyMapB = new HashMap<>();
+
+        //Map the char with the number of times they show up
+        for(int i = 0; i < a.length(); i++){
+
+            //set the case for A
+            if(frequencyMapA.containsKey(a.toLowerCase().charAt(i))) {
+                int value = frequencyMapA.get(a.toLowerCase().charAt(i)) + 1;
+                frequencyMapA.put(a.toLowerCase().charAt(i), value);
+            } else {
+                frequencyMapA.putIfAbsent(a.toLowerCase().charAt(i), 1);
+            }
+
+            //set the case for B
+            if(frequencyMapB.containsKey(b.toLowerCase().charAt(i))) {
+                int value = frequencyMapB.get(b.toLowerCase().charAt(i)) + 1;
+                frequencyMapB.put(b.toLowerCase().charAt(i), value);
+            } else {
+                frequencyMapB.putIfAbsent(b.toLowerCase().charAt(i), 1);
+            }
+        }
+
+        if(frequencyMapA.equals(frequencyMapB)) {
+            return true;
+        }
+
+        return false;
+    }
 }
 
 
