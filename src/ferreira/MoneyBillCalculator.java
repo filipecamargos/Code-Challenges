8c5ea994f398d91da8e5c7e637b4d94d3c4f6d07
filipecamargos <filipe.atrabalho@gmail.com>
2021-04-08 22:23:59 -0600

    Clean up

--- src/ferreira/MoneyBillCalculator.java
+++ src/ferreira/MoneyBillCalculator.java
@@ -6,35 +6,6 @@ public class MoneyBillCalculator {
 
         public static int[] withdraw(int amount) {
 
-//            //Value Manipulation
-//            int tempValue = amount;
-//
-//            //Create the array to hold the result set
-//           int[] bills = {0, 0, 0};
-//
-//            //100 case
-//            while(tempValue >= 100){
-//                bills[0] = bills[0] + 1;
-//                tempValue = tempValue - 100;
-//            }
-//
-//            //50 case
-//            while(tempValue >= 50 && ((tempValue % 20) != 0)){
-//                bills[1] = bills[1] + 1;
-//                tempValue = tempValue - 50;
-//            }
-//
-//            if ((tempValue % 20) == 0) {
-//                bills[2] = tempValue / 20;
-//            }
-//
-//            System.out.println(tempValue);
-//            System.out.println(bills[0]);
-//            System.out.println(bills[1]);
-//            System.out.println(bills[2]);
-
-            //20 case
-
             int[] bills = { 100, 50, 20};
 
             int[] noteCounter = { 0, 0, 0 };
