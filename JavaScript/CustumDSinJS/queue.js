1bfd492b4492e5a988665f6ee6816f8239139bb3
filipecamargos <filipe.atrabalho@gmail.com>
2021-09-10 16:57:36 -0600

    test in place

--- JavaScript/CustumDSinJS/queue.js
+++ JavaScript/CustumDSinJS/queue.js
@@ -31,7 +31,32 @@ class Queue {
     //print the elements
     print() {
         this.element.forEach(element => {
-            console.log(element);
+            console.log('\t' + element);
         });
     }
 }
+
+//test
+let myQueue = new Queue();
+console.log('add(0)');
+console.log('add(1)');
+console.log('add(2)');
+myQueue.add(0);
+myQueue.add(1);
+myQueue.add(2);
+console.log('print():');
+myQueue.print();
+console.log('peek():');
+console.log(myQueue.peek());
+console.log('remove():');
+myQueue.remove();
+console.log('print():');
+myQueue.print();
+console.log('remove():');
+console.log(myQueue.remove());
+console.log('print():');
+myQueue.print();
+console.log(`isEmpty(): ${myQueue.isEmpty()}`);
+console.log('remove():');
+console.log(myQueue.remove());
+console.log(`isEmpty(): ${myQueue.isEmpty()}`);
