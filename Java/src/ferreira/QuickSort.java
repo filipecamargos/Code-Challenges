d97004975d15f7967c2a3a90bfba28a198f231c4
filipecamargos <filipe.atrabalho@gmail.com>
2021-09-27 21:22:38 -0600

    Quick Sort

--- Java/src/ferreira/QuickSort.java
+++ Java/src/ferreira/QuickSort.java
@@ -2,7 +2,7 @@ package ferreira;
 
 public class QuickSort {
     public static void main(String[] args) {
-        int[] myArray = {10, 16, 8, 12, 15, 6, 3, 5};
+        int[] myArray = {10, 16, 8, 12, 15, 6, 3, 5, 0 , 11};
         quickSort(myArray, 0, myArray.length);
         for (int i : myArray) {
             System.out.println(i);
@@ -13,25 +13,23 @@ public class QuickSort {
     //second do the sorting with the left part and the right part dividing and coquering
 
     public static void quickSort(int[] myArray, int start, int end) {
-        //fist if we have sorted the whole elements lets return
-        if ((end - start) <= 2) {
+        if (end - start < 2) {
             return;
         }
-        
-        //create a recursive approach to find the pivot and swipe by dividing and coquering
-        int pivot = partion(myArray, start, end);
-        quickSort(myArray, start, pivot);
-        quickSort(myArray, pivot + 1, end);
+
+        int pivot = partition(myArray, start, end);
+        partition(myArray, start, pivot);
+        partition(myArray, pivot + 1, end);
     }
 
-    public static int partion(int[] myArray, int start, int end) {
+    public static int partition(int[] myArray, int start, int end) {
         int pivot = myArray[start];
         int i = start;
         int j = end;
-        
+
         while (i < j) {
             while (i < j && myArray[--j] >= pivot);
-            myArray[i] =  myArray[j];
+            myArray[i] = myArray[j];
             while (i < j && myArray[++i] <= pivot);
             myArray[j] = myArray[i];
         }
