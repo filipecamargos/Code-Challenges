6448eb01408e6e3525f0f994152346c105f71002
filipecamargos <filipe.atrabalho@gmail.com>
2021-10-14 19:52:10 -0600

    Changing Queue to take generics as type

--- Java/src/ferreira/Queue.java
+++ Java/src/ferreira/Queue.java
@@ -5,27 +5,27 @@ import java.lang.reflect.Constructor;
 import javax.swing.text.StyledEditorKit;
 
 public class Queue {
-    static class Node {
-        private int data;
+    static class Node<T> {
+        private T data;
         private Node link;
 
-        Node(int data) {
+        Node(T data) {
             this.data = data;
             this.link = null;
         }
     }
     
-    static public class MyQueue {
-        private Node head;
-        private Node tail;
+    static public class MyQueue<T> {
+        private Node<T> head;
+        private Node<T> tail;
 
         MyQueue() {
             this.head = null;
             this.tail = null;
         }
 
-        public void add(int data) {
-            Node newNode = new Node(data);
+        public void add(T data) {
+            Node<T> newNode = new Node<T>(data);
             if (this.head == null) {
                 this.head = newNode;
                 this.tail = newNode;
@@ -35,16 +35,16 @@ public class Queue {
             }
         }
 
-        public int element() {
+        public T element() {
             return this.head.data;
         }
 
-        public int remove() {
+        public T remove() {
             if (this.head == null) {
                 System.out.println("Error: Empty Queue!");
-                return -1;
+                return null;
             }
-            int data = this.head.data;
+            T data = this.head.data;
             this.head = this.head.link;
             return data;
         }
@@ -60,7 +60,7 @@ public class Queue {
     }
 
     static public void main(String[] args) {
-        MyQueue queue = new MyQueue();
+        MyQueue<Integer> queue = new MyQueue<Integer>();
         System.out.println("Adding from 1 to 10");
         for (int i = 1; i <= 10; i++) {
             queue.add(i);
@@ -75,5 +75,22 @@ public class Queue {
         }
         queue.print();
         System.out.println("Calling element(): " + queue.element());
+
+        System.out.println("_________________________________________________________________");
+        MyQueue<String> queueString = new MyQueue<String>();
+        System.out.println("Adding 10 strings");
+        for (int i = 1; i <= 10; i++) {
+            queueString.add("String: " + i);
+        }
+        queueString.print();
+        System.out.println("Calling element(): " + queueString.element());
+        System.out.println("Calling remove(): " + queueString.remove());
+        queueString.print();
+        System.out.println("Calling Removing 4 time:");
+        for (int i = 0; i < 4; i++) {
+            System.out.println("Calling remove(): " + queueString.remove());
+        }
+        queueString.print();
+        System.out.println("Calling element(): " + queueString.element());
     }
 }
