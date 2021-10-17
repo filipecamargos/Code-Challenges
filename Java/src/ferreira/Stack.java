cb6e838d742281faa590208fac817f99c10fb402
filipecamargos <filipe.atrabalho@gmail.com>
2021-10-16 21:11:03 -0600

    Stack Changed to take Templates

--- Java/src/ferreira/Stack.java
+++ Java/src/ferreira/Stack.java
@@ -5,78 +5,88 @@ import java.util.*;
 import javax.swing.text.StyledEditorKit.BoldAction;
 
 public class Stack {
-    static private class Node {
-        int data;
-        Node link;
+    static private class Node<T> {
+        T data;
+        Node<T> link;
 
-        Node(int data) {
+        Node(T data) {
             this.data = data;
             this.link = null;
         }
     }
 
-    static public class MyStack {
-        private Node top;
-        private int length;
+    static public class MyStack<T> {
+        private Node<T> top;
+        private int size;
 
         MyStack() {
             this.top = null;
-            this.length = 0;
+            this.size = 0;
         }
 
-        public void push(int data) {
-            Node newNode = new Node(data);
+        public void push(T data) {
+            Node<T> newNode = new Node<>(data);
             if (this.top == null) {
                 this.top = newNode;
-                this.top.link = null;
             } else {
                 newNode.link = this.top;
                 this.top = newNode;
             }
-
-            length++;
+            size++;
         }
 
-        public int pop() {
-            if (this.length == 0) {
-                System.out.println("Error: Trying to pop empty stack");
+        public T pop() {
+            if (this.top == null) {
+                System.out.println("Error trying to pop empty stack!");
+                return null;
             }
-            int returnedData = this.top.data;
+
+            T returnData = this.top.data;
             this.top = this.top.link;
-            this.length--;
-            return returnedData;
+            size--;
+            return returnData;
         }
 
-        public int peek() {
+        public T peek() {
+            if (this.top == null) {
+                System.out.println("Empty stack!");
+                return null;
+            }
+            
             return this.top.data;
         }
 
         public int size() {
-            return this.length;
+            return this.size;
         }
 
         public boolean isEmpty() {
-            return this.length == 0;
+            return this.top == null;
         }
 
         public void print() {
-            Node i = this.top;
-            System.out.print("\ntop : ");
+            if (this.top == null) {
+                System.out.println("Empty stack!");
+                return;
+            }
+
+            System.out.println("Top: ");
+            Node<T> i = this.top;
             while(i != null) {
-                System.out.println("\t" + i.data);
+                System.out.println(i.data);
                 i = i.link;
             }
         }
     }
     
     static public void main(String[] args) {
-        System.out.print("New stack created:");
-        MyStack stack = new MyStack();
+        System.out.println("New Integer stack created:");
+        MyStack<Integer> stack = new MyStack<>();
         System.out.println("IsEmpty(): ");
         System.out.println(stack.isEmpty());
         System.out.print("Adding from 1 to 10: ");
         for (int i = 1; i <= 10; i++) {
-            stack.push(i);;
+            stack.push(i);
         }
         System.out.print("Calling print(): ");
         stack.print();
@@ -90,6 +100,29 @@ public class Stack {
         stack.print();
         System.out.println("Calling size(): " + stack.size());
         System.out.println("IsEmpty(): ");
-        System.out.println(stack.isEmpty());        
+        System.out.println(stack.isEmpty());   
+        
+        
+        System.out.println("---------------\nNew String stack created:");
+        MyStack<String> stringStack = new MyStack<>();
+        System.out.println("IsEmpty(): ");
+        System.out.println(stringStack.isEmpty());
+        System.out.print("Adding from 1 to 10: ");
+        for (int i = 1; i <= 10; i++) {
+            stringStack.push("String " + i);
+        }
+        System.out.print("Calling print(): ");
+        stringStack.print();
+        System.out.println("Calling peek(): ");
+        System.out.println(stringStack.peek());
+        System.out.println("Calling pop() 5X: ");
+        for (int i = 0; i < 5; i++) {
+            System.out.println(stringStack.pop());
+        }
+        System.out.print("Calling print(): ");
+        stringStack.print();
+        System.out.println("Calling size(): " + stringStack.size());
+        System.out.println("IsEmpty(): ");
+        System.out.println(stringStack.isEmpty()); 
     }
 }
